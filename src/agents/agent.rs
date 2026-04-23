cffcd6bfca8f87962a5926ce73e43b51b74c2926
Citeli-py <matheus.citeli2001@gmail.com>
2026-04-23 11:53:39 -0300

    refactor: separação de chunks de assistent e de user em funções proprias handle_assistant_item e handle_user_content

--- src/agents/agent.rs
+++ src/agents/agent.rs
@@ -104,87 +104,102 @@ fn handle_chunk<R>(
     match chunk {
 
         // 🧠 Tudo vem como StreamAssistantItem
-        rig::agent::MultiTurnStreamItem::StreamAssistantItem(content) => {
-
-            match content {
-
-                // 🔹 Texto normal
-                StreamedAssistantContent::Text(text) => {
-                    print!("{text}");
-                    io::stdout().flush().unwrap();
-                    response_text.push_str(&text.text);
-                }
-
-                // 🔹 Tool sendo chamada
-                StreamedAssistantContent::ToolCall { tool_call, .. } => {
-
-                    response_text.push_str("\n[Calling tool]\n");
-                    response_text.push_str(&format!("ID: {}\n", tool_call.function.name));
-                    response_text.push_str(&format!("Args: {:?}\n", tool_call.function.arguments));
-
-                    print!("{}", response_text);
-                }
-
-                // 🔹 DELTA da tool (isso aqui é importante!)
-                StreamedAssistantContent::ToolCallDelta { content, .. } => {
-                    println!("\n[Tool delta / possível resultado]");
-                    println!("{:?}", content);
-                }
-
-                // 🔹 Reasoning
-                StreamedAssistantContent::Reasoning(reason) => {
-                    println!("\n[Reasoning]");
-                    println!("{:?}", reason.display_text());
-                }
-
-                StreamedAssistantContent::ReasoningDelta { id, reasoning } => println!("ReasoningDelta event\n{}\nEnd reasoning", reasoning),
-
-                StreamedAssistantContent::Final(_) => {}
-            }
-    
+        rig::agent::MultiTurnStreamItem::StreamAssistantItem(assistant_content) => {
+            let assistant_content_message = handle_assistant_item(assistant_content);
+            response_text.push_str(&assistant_content_message);
+            
         }
 
-        rig::agent::MultiTurnStreamItem::StreamUserItem(tool_content) => {
-
-            match tool_content {
-
-                StreamedUserContent::ToolResult {
-                    tool_result,
-                    internal_call_id,
-                } => {
-
-                    
-                    let first = tool_result.content.first();
-                    // junta first + rest
-                    let mut items = vec![&first];
-                    
-                    let rest = tool_result.content.rest();
-                    items.extend(rest.iter());
-                    
-                    let mut full_text = String::new();
-                    for item in items {
-                        match item {
-                            rig::message::ToolResultContent::Text(t) => {
-                                full_text.push_str(&t.text);
-                            }
-                            _ => {}
-                        }
-                    }
-
-                    response_text.push_str("\n[TOOL RAW OUTPUT]\n");
-                    response_text.push_str(&full_text);
-                    response_text.push('\n');
-
-                    println!("{}", response_text);
-                }
-            }
+        rig::agent::MultiTurnStreamItem::StreamUserItem(user_content) => {
+            let user_content_message = handle_user_content(user_content);
+            println!("{}", user_content_message);
+            response_text.push_str(&user_content_message);
         }
-        
 
-        rig::agent::MultiTurnStreamItem::FinalResponse(content) => {}
+        rig::agent::MultiTurnStreamItem::FinalResponse(content) => {
+            let usage = content.usage();
+            println!("\n\n[input_tokens: {}, output_tokens: {}]", usage.input_tokens, usage.output_tokens);
+        }
 
         _ => {}
     }
 
     Ok(response_text)
+}
+
+
+fn handle_user_content(user_content: StreamedUserContent) -> String{
+
+    let mut response_text = String::new();
+
+    match user_content {
+        StreamedUserContent::ToolResult { tool_result, internal_call_id,} => {
+
+            
+            let first = tool_result.content.first();
+            // junta first + rest
+            let mut items = vec![&first];
+            
+            let rest = tool_result.content.rest();
+            items.extend(rest.iter());
+            
+            let mut full_text = String::new();
+            for item in items {
+                match item {
+                    rig::message::ToolResultContent::Text(t) => {
+                        full_text.push_str(&t.text);
+                    }
+                    _ => {}
+                }
+            }
+
+            response_text.push_str("\n[TOOL RAW OUTPUT]\n");
+            response_text.push_str(&full_text);
+            response_text.push('\n');
+        }
+    }
+
+    return response_text
+}
+
+fn handle_assistant_item<R>(assistant_item: StreamedAssistantContent<R>) -> String {
+    let mut response_text = String::new();
+    match assistant_item {
+
+        // 🔹 Texto normal
+        StreamedAssistantContent::Text(text) => {
+            print!("{text}");
+            io::stdout().flush().unwrap();
+            response_text.push_str(&text.text);
+        }
+
+        // 🔹 Tool sendo chamada
+        StreamedAssistantContent::ToolCall { tool_call, .. } => {
+
+            response_text.push_str("\n[Calling tool]\n");
+            response_text.push_str(&format!("ID: {}\n", tool_call.function.name));
+            response_text.push_str(&format!("Args: {:?}\n", tool_call.function.arguments));
+
+            print!("{}", response_text);
+        }
+
+        // 🔹 DELTA da tool (isso aqui é importante!)
+        StreamedAssistantContent::ToolCallDelta { content, .. } => {
+            println!("\n[Tool delta / possível resultado]");
+            println!("{:?}", content);
+        }
+
+        // 🔹 Reasoning
+        StreamedAssistantContent::Reasoning(reason) => {
+            println!("\n[Reasoning]");
+            println!("{:?}", reason.display_text());
+        }
+
+        StreamedAssistantContent::ReasoningDelta { id, reasoning } => println!("ReasoningDelta event\n{}\nEnd reasoning", reasoning),
+
+        StreamedAssistantContent::Final(_) => {}
+    }
+
+    return response_text;
+
 }
