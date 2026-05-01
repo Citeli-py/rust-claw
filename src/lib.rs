8419ed2e7d035ae816010c879f2a4c367bc9a919
Citeli-py <matheus.citeli2001@gmail.com>
2026-04-30 21:50:00 -0300

    fix: removed pinchtab

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ pub mod tools;
 pub mod agents;
 pub mod cli;
 
-pub use tools::PinchTab;
 pub use agents::agent_factory::{AgentFactory, ModelProvider};
 pub use agents::agent_config::{AgentConfig, AgentConfigJson};
 
